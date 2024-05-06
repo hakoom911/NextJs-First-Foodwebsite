4768e39153eed3137cb015cd05e4e72e41e48f6b
hakoom911 <114762298+hakoom911@users.noreply.github.com>
2024-05-07 01:07:03 +0300

    adding uncompleted stuff for the submit shareMeal page form

--- app/meals/share/page.js
+++
@@ -1,5 +0,0 @@
-export default function ShareMealPage() {
-    return (
-        <h1>share meal page</h1>
-    );
-}
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
         <main className="not-found">
             <h1>Not found</h1>
-            <p>Unfortunately, we could not found the requested page or resource.</p>
+            <p>Unfortunately, we could not found the requested page or meal data.</p>
         </main>
     );
 }
